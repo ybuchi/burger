11c3089e70d0bf7bf3ff2e711efeda0ff9e26af0
ybuchi <yabuch_ch@hotmail.com>
2019-11-07 17:37:04 -0500

    Configured ORM. Need to make sure that the model works.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("Aussie Burger", "TRUE"),("True Swiss Burger", "FALSE"), ("The Great American Burger", "TRUE");
+VALUES ("Aussie Burger",0),("True Swiss Burger",1), ("The Great American Burger", 1);
