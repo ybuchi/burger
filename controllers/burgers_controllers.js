c3e22dfba7dc75506293f63647b6b88cba2b5068
ybuchi <yabuch_ch@hotmail.com>
2019-11-10 14:18:51 -0500

    Finished adding all the necessary files. Starting to code the logic.

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,60 @@
+//Require express and burger.js file
+var express = require("express");
+var burger = require("../models/burger.js")
+
+var router = express.Router();
+
+// Create all the routes necessary & include logic
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burger: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  }); 
+  
+  router.post("/api/cats", function(req, res) {
+    cat.create([
+      "name", "sleepy"
+    ], [
+      req.body.name, req.body.sleepy
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/cats/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    cat.update({
+      sleepy: req.body.sleepy
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  router.delete("/api/cats/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    cat.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
