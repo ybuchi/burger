2e6505aa79a600d3b1c1f51f6423bb0e2433217f
ybuchi <yabuch_ch@hotmail.com>
2019-11-11 10:36:58 -0500

    We can now display the names of the burgers in the database (aka what needs to be displayed on the left side of the scree, aka burgers that haven't been devoured yet. Now we need to make sure that the burgers that aren't devoured are displayed on the left side of the screen and the devoured burgers are displayed on the the right side of the screen.

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -9,7 +9,7 @@ router.get("/", function(req, res) {
       var hbsObject = {
         burgers: data
       };
-      console.log(hbsObject);
+      console.log("This is hbsObject: " + hbsObject);
       res.render("index", hbsObject);
     });
   }); 
